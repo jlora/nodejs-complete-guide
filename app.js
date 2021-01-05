1a858d4ca14e799000e126e69a325e0da927ae2d
JESUS ANDRES LORA CAMACHO <andres@submarine.app>
2021-01-05 13:36:46 -0700

    added express middleware into the proyect and styling screens

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-const http = require('http');
-const routes = require('./routes');
-
-console.log(routes.someText);
+const express = require('express');
+const path = require('path');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+//const bodyParser = require('body-parser');
+//const http = require('http');
+//const routes = require('./routes');
+//console.log(routes.someText);
 //const server = http.createServer(routes);
-const server = http.createServer(routes.handler);
-server.listen(3000);
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//app.use(bodyParser.urlencoded());
+//app.use((req, res, next) => {
+// console.log('In the middleware');
+// next(); // Allows the request to continue to the next middleware in line
+//});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+//const server = http.createServer(app);
+//const server = http.createServer(routes.handler);
+//server.listen(3000);
+
+app.listen(3000);
